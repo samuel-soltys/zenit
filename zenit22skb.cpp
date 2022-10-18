c51912aa7840b61868d132f7fbc120e37d84849e
Samuel Šoltys <samuelsasanka@gmail.com>
2022-10-18 09:43:04 +0200

    B

---
+++ zenit22skb.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define REP(a,b) for (int i = a; i < b; i++)
+void print_vector(vi v) { for(auto x : v) cout << x << ' '; cout << endl; }
+void print_array(int *a, int len) { REP(0, len) cout << a[i] << ' '; cout << endl; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n,inp, sum = 0;
+    cin >> n;
+
+    
+    for(int i = 0; i < n; i++) {
+        cin >> inp;
+        sum += inp;
+    }
+    cout << sum << endl;
+    
+    return 0;
+}
