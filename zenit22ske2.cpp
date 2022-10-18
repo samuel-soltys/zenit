80a6e472f9d5d2aaa528947c509a3c758721a3b8
Samuel Šoltys <samuelsasanka@gmail.com>
2022-10-18 13:00:41 +0200

    E - 7 bodov ine

---
+++ zenit22ske2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef long long ll;
+#define REP(a,b) for (int i = a; i < b; i++)
+void print_vector(vi v) { for(auto x : v) cout << x << ' '; cout << endl; }
+void print_array(int *a, int len) { REP(0, len) cout << a[i] << ' '; cout << endl; }
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int ding, dong, rozdiel, ding_v, dong_v;
+    string k;
+    cin >> ding >> dong;
+    cin >> k;
+    ll result = 0;
+    map<char, int> pismena;
+    for(int i = 0; i < k.length(); i++) {
+        if (pismena.find(k[i]) == pismena.end()) {
+            pismena.insert(pair<char, int>( k[i], 1 ));
+        } else {
+            pismena[k[i]]++;
+        }
+    }
+    pismena.erase('A');
+
+    for(int i = 0; i < 26; i++) {
+        result += min(pismena[char(i + 65)] * ding, i * dong);
+    }
+    cout << result << endl;
+    
+    return 0;
+}
